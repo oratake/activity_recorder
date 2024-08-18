3052f9277d069fe4d7ba7b387b64f9a068dfbb0a
oratake <smith.shimomura@gmail.com>
2024-08-18 16:38:34 +0900

    add: nextのexport設定 docsへの試しbuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "docs",
+  assetPrefix: "/activity_recorder",
+  basePath: "/activity_recorder",
+};
 
 export default nextConfig;
